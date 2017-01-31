8cfb3b0719b8a8b42e0e4269aad7a84ef0b01e7c
Simon Standbridge <simonstandbridge@gmail.com>
2017-01-31 21:10:43 +0000

    Created seperate "hello" page

--- Task_Week_3/testWeek3/routes/index.js
+++ Task_Week_3/testWeek3/routes/index.js
@@ -6,4 +6,9 @@ router.get('/', function(req, res, next) {
   res.render('index', { title: 'Input Table' });
 });
 
+router.get('/hello', function(req,res,next){
+  res.render('hello', {title: 'Hello'});
+});
+
+
 module.exports = router;
